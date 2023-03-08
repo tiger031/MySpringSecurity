1de81a29bf7197b7407730c7ba2554a4547247af
tiger031 <shelest100500@gmail.com>
2023-03-08 19:27:15 +0200

    Project created

---
+++ src/main/java/com/example/spring/security/MySpringSecurity/MySpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.security.MySpringSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring/security/MySpringSecurity/MySpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.security.MySpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
